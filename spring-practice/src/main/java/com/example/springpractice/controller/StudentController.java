e7edcdb9fbf5225c2c0990ba336a167f2ae03085
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2026-04-01 20:55:39 +0400

    two

---
+++ spring-practice/src/main/java/com/example/springpractice/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.example.springpractice.controller;
+
+import com.example.springpractice.model.Student;
+import com.example.springpractice.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    // GET http://localhost:8080/students
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    // POST http://localhost:8080/students
+    @PostMapping
+    public void addStudent(@RequestBody Student student) {
+        studentService.addStudent(student);
+    }
+
+
+}
